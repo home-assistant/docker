7316d04826379084876f0dd65b24ead0d0784ee3
Pascal Vizeli <pvizeli@syshack.ch>
2019-06-03 22:46:14 +0200

    add script

---
+++ generic/create_requirements.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bashio
+
+# MFA modules requirements (pip)
+MFA_MODULES=(pyotp PyQRCode)
+
+# Core components
+COMPONENTS=(frontend stream tts recorder cloud zeroconf ssdp http updater mobile_app)
+
+# IoT
+COMPONENTS+=(zwave mqtt zha proxy)
+
+REQUIREMENTS="homeassistant/requirements_default.txt"
+touch ${REQUIREMENTS}
+
+# MFA
+for module in "${MFA_MODULES[@]}"; do
+  echo "${module}" >> ${REQUIREMENTS}
+done
+
+# Components
+for component in "${COMPONENTS[@]}"; do
+    component_manifest="homeassistant/homeassistant/components/$component/manifest.json"
+    jq --raw-output '.requirements | join("\n")' "${component_manifest}" >> ${REQUIREMENTS}
+done
